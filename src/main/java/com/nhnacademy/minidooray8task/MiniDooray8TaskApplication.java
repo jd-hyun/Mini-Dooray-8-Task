ca7a8418ab12d9e5b94308696768120d9c8dd6a6
jd-hyun <wjdehdgus3540@naver.com>
2024-10-09 17:40:19 +0900

    init

---
+++ src/main/java/com/nhnacademy/minidooray8task/MiniDooray8TaskApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray8task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDooray8TaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDooray8TaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/minidooray8task/MiniDooray8TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray8task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDooray8TaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
